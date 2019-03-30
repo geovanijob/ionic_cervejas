e18e4b2c25a634dd30ffc6dd21c68e098e6c32e1
Munif Gebara Junior <munifgebarajunior@MacBook-Pro-de-Munif.local>
2019-03-30 13:32:29 -0300

    primeiro commit

---
+++ src/app/cerveja.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CervejaService {
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  public dados = [];
+
+  public carregaDados(callback) {
+    return this.httpClient.get(`https://alfa2019-mgj.firebaseio.com/cervejas.json`)
+      .toPromise().then(response => {
+        this.dados = [];
+        Object.keys(response).forEach(key => {
+          this.dados.push({
+            id: key,
+            nome: response[key].nome,
+            nota: response[key].nota
+          });
+          callback();
+        });
+      }).catch(
+        error => console.log("ERROR", error)
+      );
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CervejaService } from '../cerveja.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+
+  dadosPagina = [];
+
+
+  constructor(private service: CervejaService) {
+  }
+
+  ngOnInit(): void {
+      this.atualizar(); 
+  }
+
+  atualizar(){    
+    this.service.carregaDados(()=>this.dadosPagina=this.service.dados);
+  }
+
+}
